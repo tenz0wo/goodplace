b349e21d85231027466964607450c43cbedd71e4
tenz0wo <igor_rom7011@bk.ru>
2023-02-09 13:36:37 +0300

    --

--- APP/exort/views.py
+++ APP/exort/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.views.generic import DeleteView, UpdateView
-from .forms import CardsForm, RatingForm
+from django.contrib.auth.models import User
+from .forms import CardsForm
 from .models import Cards
 
 
@@ -15,33 +16,23 @@ class PostDeleteView(DeleteView):
     success_url = '/'
 
 def index(request):
-    us = request.user
     cards = Cards.objects.all()
     post_count = Cards.objects.count()
-
-    star_form = RatingForm()
     data = {"cards": cards, 
-            "us": us,
-            "star_form": star_form,
-            'post_count': post_count
+            'post_count': post_count,
             }
     return render(request, 'exort/cards_list.html', data)
 
 def food(request):
-    us = request.user
 
     cards = Cards.objects.all()
-    return render(request, 'exort/food.html', {"cards": cards, 'us': us})
+    return render(request, 'exort/food.html', {"cards": cards})
 
 def place(request):
-    us = request.user
-
     cards = Cards.objects.all()
-    return render(request, 'exort/place.html', {"cards": cards, 'us': us})
+    return render(request, 'exort/place.html', {"cards": cards})
 
 def create_card(request):
-    us = request.user
-
     error = ''
     if request.method == "POST":
         form = CardsForm(request.POST)
@@ -56,7 +47,6 @@ def create_card(request):
     data = {
         'form': form,
         'error': error,
-        'us': us
     }
 
     return render(request, 'exort/create_card.html', data)
--- APP/users/views.py
+++ APP/users/views.py
@@ -5,9 +5,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-
-from users.models import Profile
-
 from .forms import (LoginForm, ProfileUpdateForm, UserRegisterForm, UserUpdateForm)
 
 
@@ -22,7 +19,9 @@ def register(request):
         
     form = UserRegisterForm()
 
-    context = {'form': form}
+    context = { 
+        'form': form,
+        }
     return render(request, 'users/register.html', context)
 
 def user_login(request):
